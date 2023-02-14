23f4bcb90403a1acf60679978e2e671e67914599
Maya <salkiewicz.maya@gmail.com>
2023-02-14 13:18:47 +0100

    adding Javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-clear-all]");
+const operand = document.querySelector("[data-operand]");
+
+function addNumber(e) {
+  let button = e.target.innerText;
+  if (operand.textContent.includes(".")) return;
+  operand.innerText += button;
+}
+
+function addOperation(e) {
+  let operation = e.target.innerText;
+  operand.innerText += operation;
+}
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", addNumber);
+});
+
+operationButtons.forEach((operation) => {
+  operation.addEventListener("click", addOperation);
+});
+
+allClearButton.addEventListener("click", () => {
+  operand.innerText = "";
+});
+
+deleteButton.addEventListener("click", () => {
+  operand.textContent = operand.textContent.slice(
+    0,
+    operand.textContent.length - 1
+  );
+});
+
+equalsButton.addEventListener("click", () => {
+  operand.textContent = eval(operand.textContent);
+});
